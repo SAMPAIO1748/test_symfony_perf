bbb85512f59178e190f4a3726c828c95cd071c56
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-19 16:41:34 +0200

    auth

--- src/Entity/Commande.php
+++ src/Entity/Commande.php
@@ -47,6 +47,11 @@ class Commande
      */
     private $firstname;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="commandes")
+     */
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -120,6 +125,18 @@ class Commande
     public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
