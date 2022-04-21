e6ff4e7d74f3592edd8fa8202659d853bf140894
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-21 09:34:31 +0200

    email

--- src/Controller/Front/FrontUserController.php
+++ src/Controller/Front/FrontUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManager;
 use App\Repository\UserRepository;
@@ -10,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class FrontUserController extends AbstractController
@@ -42,6 +45,51 @@ class FrontUserController extends AbstractController
 
             $entityManagerInterface->persist($user);
             $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('home');
+        }
+        return $this->render("front/user_form.html.twig", ['userForm' => $userForm->createView()]);
+    }
+
+    /**
+     * @Route("create/user", name="create_user")
+     */
+    public function createUser(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+        UserPasswordHasherInterface $userPasswordHasherInterface,
+        MailerInterface $mailerInterface
+    ) {
+
+        $user = new User();
+
+        $userForm = $this->createForm(UserType::class, $user);
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+
+            $user->setRoles(["ROLE_USER"]);
+
+            $plainPassword = $userForm->get('password')->getData();
+            $hashedpasword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedpasword);
+
+            // Je récupère l'email entré dans le formulaire
+            $email_user = $userForm->get('email')->getData();
+
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+
+            // création de l'email
+            $email = (new Email())
+                ->from('test@test.com') // adresse d'envoie 
+                ->to($email_user) // adresse de réception
+                ->subject('inscription') // sujet du mail
+                ->html('<h1>Bienvenue chez nous</h1>'); // contenu html du mail
+
+            // envoie du mail
+            $mailerInterface->send($email);
 
             return $this->redirectToRoute('home');
         }
